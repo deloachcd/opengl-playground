e5b5f3642edcac85096f601e388a3753ee34275a
mildewchan <mildewchan@gmail.com>
2021-02-19 20:28:56 -0700

    Ensure project is compiled as C++20

--- make.sh
+++ make.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # This script just runs cmake to generate the makefiles,
 # and then make to compile them.
+export CC=/usr/bin/gcc-10
+export CXX=/usr/bin/g++-10
 cmake -G "Unix Makefiles" .
 make
