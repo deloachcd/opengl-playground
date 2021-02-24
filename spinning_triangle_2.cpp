981b8773d82a583d7f77ba15cab8b2b4502bcc61
mildewchan <mildewchan@gmail.com>
2021-02-23 21:14:21 -0700

    finally drawing the damn triangle again

--- spinning_triangle_2.cpp
+++ spinning_triangle_2.cpp
@@ -20,6 +20,44 @@ glm::dmat2x2 rotation_matrix(int theta) {
                         sin(r),  cos(r));
 }
 
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile "
+            << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+            << " shader, FUCK!" << std::endl;
+        std::cout << message << std::endl;
+    }
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader,
+        const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 void push_vec2(glm::vec2* arr_vectors, float buffer[], unsigned n_vec2) {
     for (unsigned i = 0; i < n_vec2; i++) {
         buffer[i*2] = arr_vectors[i][0];
@@ -79,6 +117,23 @@ int spin_triangle() {
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
+
+    std::string vShader = R"EOF(
+    #version 310 es
+    layout(location = 0) in vec4 position;
+    void main() {
+        gl_Position = position;
+    }
+    )EOF";
+    std::string fShader = R"EOF(
+    #version 310 es
+    layout(location = 0) out mediump vec4 color;
+    void main() {
+        color = vec4(0.0, 1.0, 1.0, 1.0);
+    }
+    )EOF";
+    unsigned int shader = CreateShader(vShader, fShader);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
